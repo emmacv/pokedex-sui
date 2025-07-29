b243587272a85864d8f6aa32268cf9285efcad7d
Emmanuel Canto <emikogears134@gmail.com>
2025-07-29 15:29:25 -0600

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Emmanuel Canto on 29/07/25.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
